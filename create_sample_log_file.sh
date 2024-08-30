00849b81c123527c4ab1e84079c7ba4798d5db55
learnitlessons <162319132+learnitlessons@users.noreply.github.com>
2024-08-30 21:14:47 +0300

    Create create_sample_log_file.sh

---
+++ create_sample_log_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "01/06/2023:07:59:59 Early morning entry
+01/06/2023:08:00:00 Start of 8 AM
+01/06/2023:08:30:45 Mid-morning entry
+01/06/2023:08:59:59 End of 8 AM
+01/06/2023:09:00:00 Start of 9 AM" > server.log
